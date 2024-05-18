e982e19ac5e4c797008d42db18f35fe859116ee9
Dave Allison <dave.allison+cstgithub@gmail.com>
2024-05-18 07:56:10 +0100

    Remove warning when reaching 5 measures

--- js/groove_writer.js
+++ js/groove_writer.js
@@ -3165,10 +3165,10 @@ function GrooveWriter() {
 
 		updateSheetMusic();
 
-		if(class_number_of_measures === 5)
-			window.alert("Please be aware that the Groove Scribe is not designed to write an entire musical score.\n" +
-						"You can create as many measures as you want, but your browser may slow down as more measures are added.\n" +
-						"There are also many notation features that would be useful for score writing that are not part of Groove Scribe");
+		// if(class_number_of_measures === 5)
+		// 	window.alert("Please be aware that the Groove Scribe is not designed to write an entire musical score.\n" +
+		// 				"You can create as many measures as you want, but your browser may slow down as more measures are added.\n" +
+		// 				"There are also many notation features that would be useful for score writing that are not part of Groove Scribe");
 	};
 
 	// add an empty measure to the front of the score
@@ -3212,10 +3212,10 @@ function GrooveWriter() {
 
 		updateSheetMusic();
 
-		if(class_number_of_measures === 5)
-			window.alert("Please be aware that the Groove Scribe is not designed to write an entire musical score.\n" +
-						"You can create as many measures as you want, but your browser may slow down as more measures are added.\n" +
-						"There are also many notation features that would be useful for score writing that are not part of Groove Scribe");
+		// if(class_number_of_measures === 5)
+		// 	window.alert("Please be aware that the Groove Scribe is not designed to write an entire musical score.\n" +
+		// 				"You can create as many measures as you want, but your browser may slow down as more measures are added.\n" +
+		// 				"There are also many notation features that would be useful for score writing that are not part of Groove Scribe");
 	};
 
 	function showHideCSS_ClassDisplay(className, force, showElseHide, showState) {
