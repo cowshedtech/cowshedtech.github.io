ddd70e73265cefacc7f8470fc03923ae2855d844
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-01-16 16:30:21 +0000

    Continue to move context menus into component structure

---
+++ vue/components/context_menus.js
@@ -0,0 +1,49 @@
+import Sticking from './context_menus/sticking.js'
+import HighHat from './context_menus/high_hat.js'
+import Tom1 from './context_menus/tom1.js'
+import Tom4 from './context_menus/tom4.js'
+import Snare from './context_menus/snare.js'
+import Kick from './context_menus/kick.js'
+import StickingsLabel from './context_menus/sticking_label.js'
+import HighHatLabel from './context_menus/highhat_label.js'
+import Tom1Label from './context_menus/tom1_label.js'
+import Tom4Label from './context_menus/tom4_label.js'
+import SnareLabel from './context_menus/snare_label.js'
+import KickLabel from './context_menus/kick_label.js'
+import PermutationLabel from './context_menus/permutation_label.js'
+import Help from './context_menus/help.js'
+import Stickings from './context_menus/Stickings.js'
+import Download from './context_menus/Download.js'
+import TimeSignature from './context_menus/time_signature.js'
+import Share from './context_menus/share.js'
+import MetronomeSpeed from './context_menus/metronome_speed.js'
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    Sticking, HighHat, Tom1, Tom4, Snare, Kick, StickingsLabel, HighHatLabel, Tom1Label, Tom4Label, SnareLabel, KickLabel, PermutationLabel, Help, Stickings, Download, TimeSignature, Share, MetronomeSpeed
+  },
+  template: `  
+    <Sticking></Sticking>
+    <HighHat></HighHat>
+    <Tom1></Tom1>
+    <Tom4></Tom4>
+    <Snare></Snare>
+    <Kick></Kick>
+    <StickingsLabel></StickingsLabel>
+    <HighHatLabel></HighHatLabel>
+    <Tom1Label></Tom1Label>
+    <Tom4Label></Tom4Label>
+    <SnareLabel></SnareLabel>
+    <KickLabel></KickLabel>
+    <PermutationLabel></PermutationLabel>
+    <Help></Help>
+    <Stickings></Stickings>
+    <Download></Download>
+    <TimeSignature></TimeSignature>
+    <Share></Share>
+    <MetronomeSpeed></MetronomeSpeed>  		
+    `
+  }
--- vue/main.js
+++ vue/main.js
@@ -1,59 +1,22 @@
 import RightHandContent from './components/right_hand_content.js'
+import ContextMenus from './components/context_menus.js'
 import MetronomeMenu from './components/metronome_menu.js'
-import Sticking from './components/context_menus/sticking.js'
-import HighHat from './components/context_menus/high_hat.js'
-import Tom1 from './components/context_menus/tom1.js'
-import Tom4 from './components/context_menus/tom4.js'
-import Snare from './components/context_menus/snare.js'
-import Kick from './components/context_menus/kick.js'
-import StickingsLabel from './components/context_menus/sticking_label.js'
-import HighHatLabel from './components/context_menus/highhat_label.js'
-import Tom1Label from './components/context_menus/tom1_label.js'
-import Tom4Label from './components/context_menus/tom4_label.js'
-import SnareLabel from './components/context_menus/snare_label.js'
-import KickLabel from './components/context_menus/kick_label.js'
-import PermutationLabel from './components/context_menus/permutation_label.js'
-import Help from './components/context_menus/help.js'
-import Stickings from './components/context_menus/Stickings.js'
-import Download from './components/context_menus/Download.js'
-import TimeSignature from './components/context_menus/time_signature.js'
-import Share from './components/context_menus/share.js'
-import MetronomeSpeed from './components/context_menus/metronome_speed.js'
-
 
 export default {
   data() {
     return {}
   },
   components: {
-    RightHandContent, MetronomeMenu, Sticking, HighHat, Tom1, Tom4, Snare, Kick, StickingsLabel, HighHatLabel, Tom1Label, Tom4Label, SnareLabel, KickLabel, PermutationLabel, Help, Stickings, Download, TimeSignature, Share, MetronomeSpeed
+    RightHandContent, ContextMenus, MetronomeMenu
   },
   template: `
     <RightHandContent></RightHandContent>		
     <MetronomeMenu></MetronomeMenu>
-	<Sticking></Sticking>
-	<HighHat></HighHat>
-	<Tom1></Tom1>
-	<Tom4></Tom4>
-	<Snare></Snare>
-	<Kick></Kick>
-	<StickingsLabel></StickingsLabel>
-	<HighHatLabel></HighHatLabel>
-	<Tom1Label></Tom1Label>
-	<Tom4Label></Tom4Label>
-	<SnareLabel></SnareLabel>
-	<KickLabel></KickLabel>
-	<PermutationLabel></PermutationLabel>
-	<Help></Help>
-	<Stickings></Stickings>
-	<Download></Download>
-	<TimeSignature></TimeSignature>
-	<Share></Share>
-	<MetronomeSpeed></MetronomeSpeed>
+	  <ContextMenus></ContextMenus>	
     <div id="grooveListWrapper">
-		<script>
-			document.write(grooves.getGroovesAsHTML());
-		</script>
-	</div>
+		  <script>
+			  document.write(grooves.getGroovesAsHTML());
+		  </script>
+	  </div>
 	`
   }
