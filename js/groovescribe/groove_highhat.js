b58ea2a602c6634fb21a4b3ec5533239637bba8e
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-01-24 16:10:45 +0000

    Continue to move into components

---
+++ js/groovescribe/groove_highhat.js
@@ -0,0 +1,148 @@
+// Javascript for the Groove Scribe HTML application
+
+// local constants
+
+
+var constant_note_on_color_hex = "#000000"; // black
+var constant_note_on_color_rgb = 'rgb(0, 0, 0)'; // black
+var constant_hihat_note_off_color_hex = "#CCC";
+var constant_hihat_note_off_color_rgb = 'rgb(204, 204, 204)'; // grey
+var constant_note_hidden_color_rgb = "transparent";
+
+//
+// TODO move to generic location
+//
+function play_single_note_for_note_setting(note_val) {
+    const midiSystem = MIDI.WebAudio || MIDI.AudioTag;
+    if (midiSystem) {
+        midiSystem.noteOn(9, note_val, constant_OUR_MIDI_VELOCITY_NORMAL, 0);
+    }
+}
+
+function is_hh_on(id) {
+    return get_hh_state(id, "ABC") !== false;
+}
+
+// returns the ABC notation for the HH state
+// false = off
+// see the top constants for mappings
+function get_hh_state(id, returnType) {
+    // Validate returnType
+    if (returnType !== "ABC" && returnType !== "URL") {
+        console.log("bad returnType in get_hh_state()");
+        returnType = "ABC";
+    }
+
+    // Define mapping of element IDs to return values
+    const stateMap = [
+        { id: "hh_ride", abc: constant_ABC_HH_Ride, url: "r" },
+        { id: "hh_ride_bell", abc: constant_ABC_HH_Ride_Bell, url: "b" },
+        { id: "hh_cow_bell", abc: constant_ABC_HH_Cow_Bell, url: "m" },
+        { id: "hh_crash", abc: constant_ABC_HH_Crash, url: "c" },
+        { id: "hh_stacker", abc: constant_ABC_HH_Stacker, url: "s" },
+        { id: "hh_metronome_normal", abc: constant_ABC_HH_Metronome_Normal, url: "n" },
+        { id: "hh_metronome_accent", abc: constant_ABC_HH_Metronome_Accent, url: "N" },
+        { id: "hh_open", abc: constant_ABC_HH_Open, url: "o" },
+        { id: "hh_close", abc: constant_ABC_HH_Close, url: "+" },
+        { id: "hh_accent", abc: constant_ABC_HH_Accent, url: "X" },
+        { id: "hh_cross", abc: constant_ABC_HH_Normal, url: "x" }
+    ];
+
+    // Find first active state
+    const activeState = stateMap.find(state => 
+        document.getElementById(state.id + id).style.color === constant_note_on_color_rgb
+    );
+
+    // Return appropriate value based on returnType
+    if (activeState) {
+        return returnType === "ABC" ? activeState.abc : activeState.url;
+    }
+
+    // Return default value if no active state found
+    return returnType === "ABC" ? false : "-";
+}
+
+// TODO: refactor this using a lookup table of constants
+function set_hh_state(id, mode, make_sound) {
+
+    // hide everything optional
+    document.getElementById("hh_cross" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_ride" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_ride_bell" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_cow_bell" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_crash" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_stacker" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_metronome_normal" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_metronome_accent" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_open" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_close" + id).style.color = constant_note_hidden_color_rgb;
+    document.getElementById("hh_accent" + id).style.color = constant_note_hidden_color_rgb;
+
+    // turn stuff on conditionally
+    switch (mode) {
+        case "off":
+            document.getElementById("hh_cross" + id).style.color = constant_hihat_note_off_color_hex;
+            break;
+        case "normal":
+            document.getElementById("hh_cross" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_NORMAL);
+            break;
+        case "ride":
+            document.getElementById("hh_ride" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_RIDE);
+            break;
+        case "ride_bell":
+            document.getElementById("hh_ride_bell" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_RIDE_BELL);
+            break;
+        case "cow_bell":
+            document.getElementById("hh_cow_bell" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_COW_BELL);
+            break;
+        case "crash":
+            document.getElementById("hh_crash" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_CRASH);
+            break;
+        case "stacker":
+            document.getElementById("hh_stacker" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_STACKER);
+            break;
+        case "metronome_normal":
+            document.getElementById("hh_metronome_normal" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_METRONOME_NORMAL);
+            break;
+        case "metronome_accent":
+            document.getElementById("hh_metronome_accent" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_METRONOME_ACCENT);
+            break;
+        case "open":
+            document.getElementById("hh_cross" + id).style.color = constant_note_on_color_hex;
+            document.getElementById("hh_open" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_OPEN);
+            break;
+        case "close":
+            document.getElementById("hh_cross" + id).style.color = constant_note_on_color_hex;
+            document.getElementById("hh_close" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_NORMAL);
+            break;
+        case "accent":
+            document.getElementById("hh_cross" + id).style.color = constant_note_on_color_hex;
+            document.getElementById("hh_accent" + id).style.color = constant_note_on_color_hex;
+            if (make_sound)
+                play_single_note_for_note_setting(constant_OUR_MIDI_HIHAT_ACCENT);
+            break;
+        default:
+            console.log("bad switch in set_hh_state");
+            break;
+    }
+}
---
+++ js/groovescribe/groove_tom.js
@@ -0,0 +1,114 @@
+// Javascript for the Groove Scribe HTML application
+
+var constant_default_tempo = 80;
+var constant_note_stem_off_color = "transparent";
+var constant_note_on_color_hex = "#000000"; // black
+var constant_note_on_color_rgb = 'rgb(0, 0, 0)'; // black
+var constant_note_off_color_hex = "#FFF";
+var constant_note_off_color_rgb = 'rgb(255, 255, 255)'; // white
+var constant_note_border_color_hex = "#999";
+var constant_hihat_note_off_color_hex = "#CCC";
+var constant_hihat_note_off_color_rgb = 'rgb(204, 204, 204)'; // grey
+var constant_note_hidden_color_rgb = "transparent";
+var constant_sticking_right_on_color_rgb = "rgb(36, 132, 192)";
+var constant_sticking_left_on_color_rgb = "rgb(57, 57, 57)";
+var constant_sticking_both_on_color_rgb = "rgb(57, 57, 57)";
+var constant_sticking_count_on_color_rgb = "rgb(57, 57, 57)";
+var constant_sticking_right_off_color_rgb = "rgb(204, 204, 204)";
+var constant_sticking_left_off_color_rgb = "rgb(204, 204, 204)";
+var constant_snare_accent_on_color_hex = "#FFF";
+var constant_snare_accent_on_color_rgb = "rgb(255, 255, 255)";
+
+//
+// TODO move to generic location
+//
+function play_single_note_for_note_setting(note_val) {
+    const midiSystem = MIDI.WebAudio || MIDI.AudioTag;
+    if (midiSystem) {
+        midiSystem.noteOn(9, note_val, constant_OUR_MIDI_VELOCITY_NORMAL, 0);
+    }
+}
+
+
+function is_tom_on(id, tom_num) {
+    var state = get_tom_state(id, tom_num, "ABC");
+
+    if (state !== false)
+        return true;
+
+    return false;
+}
+
+// returns the ABC notation for the Tom state
+// false = off
+// "x" = normal tom
+function get_tom_state(id, tom_num, returnType) {
+
+    var tomOn = (document.getElementById("tom_circle" + tom_num + "-" + id).style.backgroundColor == constant_note_on_color_rgb);
+
+    if (returnType != "ABC" && returnType != "URL") {
+        console.log("bad returnType in get_kick_state()");
+        returnType = "ABC";
+    }
+
+    if (tomOn) {
+        if (returnType == "ABC")
+            switch (tom_num) {
+                case 1:
+                    return constant_ABC_T1_Normal; // normal
+                    break;
+                case 4:
+                    return constant_ABC_T4_Normal; // normal
+                    break;
+                default:
+                    console.log("bad switch in get_tom_state. bad tom num:" + tom_num);
+                    break;
+            }
+        else if (returnType == "URL")
+            return "x"; // normal
+    }
+
+    if (returnType == "ABC")
+        return false; // off (rest)
+    else if (returnType == "URL")
+        return "-"; // off (rest)
+}
+
+// set the tom note on with type
+function set_tom_state(id, tom_num, mode, make_sound) {
+    // turn stuff on conditionally
+    switch (mode) {
+        case "off":
+            document.getElementById("tom_circle" + tom_num + "-" + id).style.backgroundColor = constant_note_off_color_hex;
+            document.getElementById("tom_circle" + tom_num + "-" + id).style.borderColor = constant_note_border_color_hex;
+            break;
+        case "normal":
+            document.getElementById("tom_circle" + tom_num + "-" + id).style.backgroundColor = constant_note_on_color_hex;
+            document.getElementById("tom_circle" + tom_num + "-" + id).style.borderColor = constant_note_border_color_hex;
+            if (make_sound)
+                switch (tom_num) {
+                    case 1:
+                        play_single_note_for_note_setting(constant_OUR_MIDI_TOM1_NORMAL);
+                        break;
+                    case 4:
+                        play_single_note_for_note_setting(constant_OUR_MIDI_TOM4_NORMAL);
+                        break;
+                    default:
+                        console.log("bad switch in set_tom_state. bad tom num:" + tom_num);
+                        break;
+                }
+            break;
+        default:
+            console.log("bad switch in set_tom_state");
+            break;
+    }
+}
+
+// silly helpers, but needed for argument compatibility with the other set states
+function set_tom1_state(id, mode, make_sound) {
+    set_tom_state(id, 1, mode, make_sound);
+}
+
+function set_tom4_state(id, mode, make_sound) {
+    set_tom_state(id, 4, mode, make_sound);
+}
