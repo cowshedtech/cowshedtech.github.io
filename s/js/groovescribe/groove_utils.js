e85cf408e90197b55e9adfaa28d7e0202e218919
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-03-06 11:32:49 +0000

    Move keyboard stuff out

--- s/js/groovescribe/groove_utils.js
+++ s/js/groovescribe/groove_utils.js
@@ -116,43 +116,7 @@ function GrooveUtils() {
 
 	
 
-	function setupHotKeys() {
-
-		var isCtrl = false;
-		document.onkeyup = function (e) {
-			if (e.which == 17)
-				isCtrl = false;
-		};
-
-		document.onkeydown = function (e) {
-			if (e.which == 17)
-				isCtrl = true;
-			/*
-			if(e.which == 83 && isCtrl == true) {
-			alert('CTRL-S pressed');
-			return false;
-			}
-			 */
-			// only accept the event if it not going to an INPUT field
-			// otherwise we can't use spacebar in text fields :(
-			if (e.which == 32 && (e.target.type == "range" || (e.target.tagName.toUpperCase() != "INPUT" && e.target.tagName.toUpperCase() != "TEXTAREA"))) {
-
-				// spacebar
-				root.startOrStopMIDI_playback();
-				return false;
-			}
-			if (e.which == 179) {
-				// Play button
-				root.startOrPauseMIDI_playback();
-			}
-			if (e.which == 178) {
-				// Stop button
-				root.stopMIDI_playback();
-			}
-
-			return true;
-		};
-	}
+	
 
 	
 
@@ -314,7 +278,7 @@ function GrooveUtils() {
 			document.getElementById("midiPlayImage" + root.grooveUtilsUniqueIndex).onclick = function (event) {
 				root.startOrStopMIDI_playback();
 			}; // enable play button
-			setupHotKeys(); // spacebar to play
+			setupHotKeys(root); // spacebar to play
 		};
 	};
 	root.midiEventCallbacks = new root.midiEventCallbackClass(root);
