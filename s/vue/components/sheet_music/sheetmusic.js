3d53e9819208dd459e53c7c06d35915b42825e29
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-03-25 12:40:23 +0000

    Update sheetmusic.js

--- s/vue/components/sheet_music/sheetmusic.js
+++ s/vue/components/sheet_music/sheetmusic.js
@@ -33,14 +33,12 @@ class SheetMusic {
 
         // called by generate_ABC to remake the sheet music on the page
     displayNewSVG() {
-        var svgTarget = document.getElementById("svgTarget"),
-            diverr = document.getElementById("diverr");
+        var diverr = document.getElementById("diverr");
 
         var abc_source = document.getElementById("ABCsource").value;
         var svg_return = renderABCtoSVG(editor.track, abc_source);
 
-        diverr.innerHTML = svg_return.error_html;
-        // svgTarget.innerHTML = svg_return.svg;
+        diverr.innerHTML = svg_return.error_html;        
         
         this.setSVG(svg_return.svg)        
     };
