c69858aac4a9939a4c4da9f94c4536b7dbd4d194
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-03-29 09:59:46 +0000

    Update menu.js

--- s/vue/components/options/vue/menu.js
+++ s/vue/components/options/vue/menu.js
@@ -1,12 +1,12 @@
 export default {
-  
-  data() {
-    return {
-		highlight: options ? options.isHighlightOn() : false		
-	 }
-  },
-  
-  props: {
+
+	data() {
+		return {
+			highlight: options ? options.isHighlightOn() : false
+		}
+	},
+
+	props: {
 		isOpen: {
 			type: Boolean,
 			default: false
@@ -19,35 +19,23 @@ export default {
 		}
 	},
 
-  watch: {
-		isOpen() {
-			console.log(`here`)
-			console.log(this.isOpen)
-			console.log(this.x)
-			console.log(this.y)
-      		// options.optionsAnchorClick()
-		}
+	methods: {
+		handleToggleHighlight() {
+			let higlightOn = options.isHighlightOn();
+			options.setHighlightOn(!higlightOn)
+		},
 	},
 
-	methods: {
-        handleToggleHighlight() {
-			let higlightOn = options.isHighlightOn();
-			options.setHighlightOn(!higlightOn)            
-        },        
-    },
-
 	mounted() {
-        // Subscribe to metronome changes
-        this.removeHandler = options?.addChangeHandler(() => {
-            this.highlight = options.isHighlightOn()
-        })
-    },
-    beforeUnmount() {
-        // Cleanup event handler
-        if (this.removeHandler) this.removeHandler() 
-    },
+		this.removeHandler = options?.addChangeHandler(() => {
+			this.highlight = options.isHighlightOn()
+		})
+	},
+	beforeUnmount() {
+		if (this.removeHandler) this.removeHandler()
+	},
 
-  template: `
+	template: `
 	<span class="noteContextMenuNew" v-if="isOpen" :style="{ top: y + 'px', left: x + 'px' }">
 		<ul id="optionsContextMenu" class="list">
 			<li 
