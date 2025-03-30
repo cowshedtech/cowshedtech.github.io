19b1ba39d1e16919a86c652040d27b6285d5a65a
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-03-30 13:18:38 +0100

    ...

--- s/vue/main.js
+++ s/vue/main.js
@@ -1,4 +1,4 @@
-import RightHandContent from './components/right_hand_content.js'
+import RightHandContent from './components/TheRightHandContent/vue/right_hand_content.js'
 import TimeSignature from './components/time_signature/vue/picker.js'
 import TopNavigation from './components/TheNavigationTop/vue/top_navigation.js'
 import ContextMenus from './components/context_menus/context_menus.js'
