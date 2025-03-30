8d56903de6d9f78bb6929ca650b7ae3e207753f2
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-03-30 13:20:28 +0100

    Update the_navigation_bottom.js

--- s/vue/components/TheNavigationBottom/vue/the_navigation_bottom.js
+++ s/vue/components/TheNavigationBottom/vue/the_navigation_bottom.js
@@ -1,9 +1,9 @@
 import PrintButton from '../../print/vue/print.js'
-import DownloadButton from '../../download/vue/button.js'
+import DownloadButton from '../../Download/vue/button.js'
 import ShareButton from '../../share/vue/button.js'
 import StickingsButton from '../../options/vue/stickings_button.js'
 import TomsButton from '../../options/vue/toms_button.js'
-import ClearButton from '../../clear/vue/button.js'
+import ClearButton from '../../Clear/vue/button.js'
 
 export default {
   components: {
