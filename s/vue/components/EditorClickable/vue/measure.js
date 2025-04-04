d7345b2207c4a8ebf480e9800f8c61491ab2e7a8
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-04-04 09:40:36 +0100

    ..

--- s/vue/components/EditorClickable/vue/measure.js
+++ s/vue/components/EditorClickable/vue/measure.js
@@ -1,6 +1,42 @@
 import { ref } from 'vue'
 import MeasureButtonAddStart from './measure_button_add_start.js'
 import MeasureControls from './measure_controls.js'
+import Sticking from './sticking.js'
+
+
+function generateStickingContainerHTML3(baseindex, indexStartForNotes, notesPerMeasure, numBeats, noteValuePerMeasure) {
+  var newHTML = [];
+
+  // newHTML.push('<div class="stickings-row-container">');
+  newHTML.push('  <div class="line-labels">');
+  newHTML.push('     <div class="stickings-label" onClick="noteLabelClick(event, \'stickings\', ' + baseindex + ')" oncontextmenu="event.preventDefault(); noteLabelClick(event, \'stickings\', ' + baseindex + ')">STICKINGS</div>');
+  newHTML.push('  </div>');
+  newHTML.push('  <div class="music-line-container">');
+  newHTML.push('     <div class="notes-container">');
+  newHTML.push('       <div class="stickings-container">');
+  newHTML.push('         <div class="opening_note_space"> </div>');
+
+  for (var i = indexStartForNotes; i < notesPerMeasure + indexStartForNotes; i++) {
+      newHTML.push('       <div id="sticking' + i + '" class="sticking">');
+      newHTML.push('         <div class="sticking_right note_part" id="sticking_right' + i + '" onClick="noteLeftClick(event, \'sticking\', ' + i + ')" oncontextmenu="event.preventDefault(); noteRightClick(event, \'sticking\', ' + i + ')" onmouseenter="noteOnMouseEnter(event, \'sticking\')">R</div>');
+      newHTML.push('         <div class="sticking_left note_part" id="sticking_left' + i + '" onClick="noteLeftClick(event, \'sticking\', ' + i + ')" oncontextmenu="event.preventDefault(); noteRightClick(event, \'sticking\', ' + i + ')">L</div>');
+      newHTML.push('         <div class="sticking_both note_part" id="sticking_both' + i + '" onClick="noteLeftClick(event, \'sticking\', ' + i + ')" oncontextmenu="event.preventDefault(); noteRightClick(event, \'sticking\', ' + i + ')">R/L</div>');
+      newHTML.push('         <div class="sticking_count note_part" id="sticking_count' + i + '" onClick="noteLeftClick(event, \'sticking\', ' + i + ')" oncontextmenu="event.preventDefault(); noteRightClick(event, \'sticking\', ' + i + ')">C</div>');
+      newHTML.push('       </div>');
+
+      // add space between notes, except on the last note
+      if ((i - (indexStartForNotes - 1)) % noteGroupingSize(notesPerMeasure, numBeats, noteValuePerMeasure) === 0 && i < notesPerMeasure + indexStartForNotes - 1) {
+          newHTML.push(' <div class="space_between_note_groups"> </div>');
+      }
+  }
+  newHTML.push('       <div class="end_note_space"></div>');
+  newHTML.push('     </div>');
+  newHTML.push('   </div>');
+  // newHTML.push('</div>');
+
+  return newHTML.join(''); // Join the array into a single string
+}
+
 
 /**
  * Creates HTML for a music staff container including note grids and labels.
@@ -19,7 +55,7 @@ function htmlForStaffContainer2(baseindex, indexStartForNotes) {
   const parts = [];
 
   // Add sticking container
-  parts.push(generateStickingContainerHTML(baseindex, indexStartForNotes, notesPerMeasure, numBeats, noteValue));
+  // parts.push(generateStickingContainerHTML2(baseindex, indexStartForNotes, notesPerMeasure, numBeats, noteValue));
 
   // Notes row container with line labels
   parts.push(`
@@ -70,6 +106,15 @@ function generateWriterHTML() {
   return genHTML
 }
 
+//
+//
+function generateStickingHTML() {
+  var genHTML = "";
+  const { notesPerMeasure, numBeats, noteValue } = editor.track;
+  
+  return generateStickingContainerHTML3(1, 0, notesPerMeasure, numBeats, noteValue);
+}
+
 
 //
 //
@@ -82,18 +127,22 @@ export default {
   },
 
   components: {
-    MeasureButtonAddStart, MeasureControls
+    MeasureButtonAddStart, MeasureControls, Sticking
   },
 
   setup() {
     const content = ref("")
     content.value = generateWriterHTML();
-    return { content }
+
+    const stickingContent = ref("")
+    stickingContent.value = generateStickingHTML();
+    return { content, stickingContent }
   },
 
   template: `
     <MeasureButtonAddStart></MeasureButtonAddStart>
     <div class="staff-container" id="staff-container1">
+      <Sticking></Sticking>
       <span v-html="content"></span>
     </div>    
     <MeasureControls></MeasureControls>    
