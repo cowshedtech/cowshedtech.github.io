8018c233613b2df36090b6ee4b97177b65efed58
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-04-06 10:26:42 +0100

    ..

---
+++ s/vue/components/EditorClickable/vue/label_menu_tom.js
@@ -0,0 +1,42 @@
+
+export default {
+	props: {
+		tomIndex: {
+			type: Number,
+			required: true
+		},
+        measureIndex: {
+            type: Number,
+            required: true
+        },
+        isOpen: {
+            type: Boolean,
+            default: false
+        },
+        x: {
+            type: String
+        },
+        y: {
+            type: String
+        }
+	},
+
+	methods: {
+		handleClick(action) {
+            noteLabelPopupClick("tom" + this.tomIndex, action, this.measureIndex)
+            this.$emit('close')
+        }
+    },
+
+	template: `
+	<div class="noteContextMenuNew" v-if="isOpen" :style="{ top: y + 'px', left: x + 'px' }">
+		<ul :id="'tom' + tomIndex + 'LabelContextMenu'" class="list">
+			<li @click="handleClick('all_of')">all Toms <b>Off</b></li>
+			<li @click="handleClick('all_on')">all Toms <b>On</b></li>
+			<li id='mute_tom1_menu_item' @click="handleClick('mute')">mute tom sound</li>
+			<li @click="handleClick('cancel')">cancel</li>
+		</ul>
+	</div>
+`
+}
+
--- s/vue/components/EditorClickable/vue/line_labels.js
+++ s/vue/components/EditorClickable/vue/line_labels.js
@@ -1,20 +1,23 @@
 import HighHatLabelMenu from "./label_menu_highhat.js"
 import KickLabelMenu from "./label_menu_kick.js"
 import SnareLabelMenu from "./label_menu_snare.js"
+import TomLabelMenu from "./label_menu_tom.js"
 
 export default {
   data() {
     return {
       labels: [
         { instrument: 'hh', class: 'hh-label', id: 'hh-label', text: 'Hi-hat' },
-        { instrument: 'tom', class: 'tom-label', id: 'tom1-label', text: 'Tom' },
+        { instrument: 'tom1', class: 'tom-label', id: 'tom1-label', text: 'Tom' },
         { instrument: 'snare', class: 'snare-label', id: 'snare-label', text: 'Snare' },
-        { instrument: 'tom', class: 'tom-label', id: 'tom4-label', text: 'Tom' },
+        { instrument: 'tom4', class: 'tom-label', id: 'tom4-label', text: 'Tom' },
         { instrument: 'kick', class: 'kick-label', id: 'kick-label', text: 'Kick' }
       ],
       isHighHatPopupOpen: false,
       isKickPopupOpen: false,
       isSnarePopupOpen: false,
+      isTom1PopupOpen: false,
+      isTom4PopupOpen: false,
       menuX: 0,
       menuY: 0,      
     }
@@ -28,7 +31,7 @@ export default {
   },
 
   components: {
-    HighHatLabelMenu, KickLabelMenu, SnareLabelMenu
+    HighHatLabelMenu, KickLabelMenu, SnareLabelMenu, TomLabelMenu
   },
 
   methods: {
@@ -36,12 +39,16 @@ export default {
       if (instrument === "hh") this.isHighHatPopupOpen = !this.isHighHatPopupOpen;
       if (instrument === "kick") this.isKickPopupOpen = !this.isKickPopupOpen;
       if (instrument === "snare") this.isSnarePopupOpen = !this.isSnarePopupOpen;
+      if (instrument === "tom1") this.isTom1PopupOpen = !this.isTom1PopupOpen;
+      if (instrument === "tom4") this.isTom4PopupOpen = !this.isTom4PopupOpen;
     },
 
     closeMenu() {
       this.isHighHatPopupOpen = false;
       this.isKickPopupOpen = false;
       this.isSnarePopupOpen = false;
+      this.isTom1PopupOpen = false;
+      this.isTom4PopupOpen = false;      
     },
 
     handleClick(event, instrument) {
@@ -82,6 +89,22 @@ export default {
         :y="menuY"
         @close="closeMenu">
       </SnareLabelMenu>
+      <TomLabelMenu
+        :tomIndex="1"
+        :measureIndex="measureIndex"
+        :is-open="isTom1PopupOpen" 
+        :x="menuX" 
+        :y="menuY"
+        @close="closeMenu">
+      </TomLabelMenu>
+      <TomLabelMenu
+        :tomIndex="2"
+        :measureIndex="measureIndex"
+        :is-open="isTom4PopupOpen" 
+        :x="menuX" 
+        :y="menuY"
+        @close="closeMenu">
+      </TomLabelMenu>
     </div>    
   `
 }
