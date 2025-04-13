b758d4a59b53c6103ef273402e95ae06e2e48edf
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-04-13 19:31:12 +0100

    ..

--- s/vue/components/EditorClickable/vue/kick.js
+++ s/vue/components/EditorClickable/vue/kick.js
@@ -1,3 +1,5 @@
+import Menu from "./kick_menu.js"
+
 export default {
   props: {
     track: {
@@ -18,7 +20,10 @@ export default {
         KICK_NORMAL: constant_ABC_KI_Normal,
         KICK_SPLASH: constant_ABC_KI_Splash,
         KICK_SPLASH_AND_KICK: constant_ABC_KI_SandK        
-      }
+      },
+      isPopupOpen: false,
+      menuX: 0,
+      menuY: 0,
     }
   },
 
@@ -37,11 +42,25 @@ export default {
         this.track.setKickState(this.noteIndex, newMode, true);                
     },
     handleRightClick(event) {
-        noteRightClick(event, 'kick', this.noteIndex)
+      this.menuX = event.clientX;
+      this.menuY = event.clientY;
+      this.isPopupOpen = true;
     },
     handleMouseEnter(event) {
-        noteOnMouseEnter(event, 'kick', this.noteIndex)
+      let action = null;
+      if (event.ctrlKey) action = "on";
+      if (event.altKey) action = "off";  
+      if (action)
+        this.track.setKickState(this.noteIndex, action == "off" ? constant_ABC_OFF : constant_ABC_KI_Normal, true);     
     },
+    handleAction(action) {
+      this.track.setKickState(this.noteIndex, action, true);  
+      this.isPopupOpen = false;
+    }
+  },
+
+  components: {
+    Menu
   },
 
   template: `
@@ -49,6 +68,12 @@ export default {
         <div v-if="noteABC === constants.KICK_SPLASH || noteABC === constants.KICK_SPLASH_AND_KICK" class="kick_splash note_part" style="color: #000000" :id="'kick_splash' + noteIndex"><i class="fa fa-times"></i></div>
         <div v-if="noteABC === constants.KICK_NORMAL || noteABC === constants.KICK_SPLASH_AND_KICK" class="kick_circle note_part" style="backgroundColor: #000000; borderColor: #999" :id="'kick_circle' + noteIndex"></div>
         <div v-if="noteABC === constants.KICK_OFF" class="kick_circle note_part" style="borderColor: #999" :id="'kick_circle' + noteIndex"></div>
+        <Menu
+          :is-open="isPopupOpen" 
+          :x="menuX" 
+          :y="menuY"
+          @action="handleAction">
+        </Menu>
     </div>
   `,
 }
--- s/vue/components/EditorClickable/vue/kick_menu.js
+++ s/vue/components/EditorClickable/vue/kick_menu.js
@@ -2,22 +2,52 @@ import InstrumentMenu from './base_instrument_menu.js'
 
 export default {
 	name: 'KickMenu',
+	
+	props: {
+		isOpen: {
+			type: Boolean,
+			default: false
+		},
+		x: {
+			type: String
+		},
+		y: {
+			type: String
+		}
+	},
+
 	components: {
 		InstrumentMenu
 	},
+	
 	data() {
     	return { 
-		modes: [
-			{ newState: 'off', label: 'Off' },
-			{ newState: 'normal', label: 'Kick Normal' },
-			{ newState: 'splash', label: 'Hi-hat foot' },
-			{ newState: 'kick_and_splash', label: 'Kick & Hi-hat foot' },			
-		],
-		instrument: "kick",
-		element: "kickContextMenu",
-	}	
-  },
-  template: `
-	<InstrumentMenu :instrument="instrument" :element="element" :instrument-modes="modes"></InstrumentMenu>
-  `
+			modes: [
+				{ newState: constant_ABC_OFF, label: 'Off' },
+				{ newState: constant_ABC_KI_Normal, label: 'Kick Normal' },
+				{ newState: constant_ABC_KI_Splash, label: 'Hi-hat foot' },
+				{ newState: constant_ABC_KI_SandK, label: 'Kick & Hi-hat foot' },			
+			],
+			instrument: "kick",
+			element: "kickContextMenu",
+		}	
+	},
+
+	methods: {
+		handleAction(action) {
+			this.$emit('action', action)
+		}
+	},
+  
+	template: `
+		<InstrumentMenu 
+			:is-open="isOpen" 
+			:x="x" 
+			:y="y"
+			@action="closeMenu"
+			:instrument="instrument" 
+			:element="element" 
+			:instrument-modes="modes">
+		</InstrumentMenu>
+  	`
 }
