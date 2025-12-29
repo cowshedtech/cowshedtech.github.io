5da01d11249bcc56f197c783ad93150287d5b020
Dave Allison <dave.allison+cstgithub@gmail.com>
2025-12-29 12:14:38 +0000

    Continue with mute functionality

--- s/vue/helpers/midi_creater.js
+++ s/vue/helpers/midi_creater.js
@@ -1,3 +1,55 @@
+// each of the instruments can be muted.   Check the UI and zero out the array if the instrument is marked as muted
+// for a particular measure
+function muteArrayFromClickableUI(Sticking_Array, HH_Array, Snare_Array, Kick_Array, Toms_Array, measureIndex) {
+    
+    if (editor.track.isInstrumentMutedInMeasure(Instruments.HIGH_HAT, measureIndex))
+        fill_array_with_value_false(HH_Array, HH_Array.length);
+    if (editor.track.isInstrumentMutedInMeasure(Instruments.TOM1, measureIndex))
+        fill_array_with_value_false(Toms_Array[0], Toms_Array[0].length);
+    if (editor.track.isInstrumentMutedInMeasure(Instruments.SNARE, measureIndex))
+        fill_array_with_value_false(Snare_Array, Snare_Array.length);
+    if (editor.track.isInstrumentMutedInMeasure(Instruments.TOM4, measureIndex))
+        fill_array_with_value_false(Toms_Array[3], Toms_Array[3].length);
+    if (editor.track.isInstrumentMutedInMeasure(Instruments.KICK, measureIndex))
+        fill_array_with_value_false(Kick_Array, Kick_Array.length);    
+}
+
+
+// query the clickable UI and generate a 32 element array representing the notes of one measure
+// note: the ui may have fewer notes, but we scale them to fit into the 32 elements proportionally
+// If using triplets returns 48 notes.   Otherwise always 32.
+//
+// (note: Only one measure, not all the notes on the page if multiple measures are present)
+// Return value is the number of notes.
+function get32NoteArrayFromClickableUI(Sticking_Array, HH_Array, Snare_Array, Kick_Array, Toms_Array, startIndexForClickableUI) {
+
+    var scaler = getNoteScaler(editor.track.notesPerMeasure, editor.track.numBeats, editor.track.noteValue); // fill proportionally
+
+    // fill in the arrays from the clickable UI
+    for (var i = 0; i < editor.track.notesPerMeasure; i++) {
+        var array_index = (i) * scaler;
+
+        // only grab the stickings if they are visible
+        // if (options.isStickingVisible())
+        //     Sticking_Array[array_index] = editor.track.getStickingState(i + startIndexForClickableUI, "ABC");
+
+        HH_Array[array_index] = editor.track.getInstrumentNote(Instruments.HIGH_HAT, i + startIndexForClickableUI);
+
+        if (options.areTomsVisible()) {
+            Toms_Array[0][array_index] = editor.track.getInstrumentNote(Instruments.TOM1, i + startIndexForClickableUI, "ABC");
+            Toms_Array[3][array_index] = editor.track.getInstrumentNote(Instruments.TOM4, i + startIndexForClickableUI, "ABC");
+
+        }
+
+        Snare_Array[array_index] = editor.track.getInstrumentNote(Instruments.SNARE, i + startIndexForClickableUI);
+        Kick_Array[array_index] = editor.track.getInstrumentNote(Instruments.KICK, i + startIndexForClickableUI);       
+
+    }
+
+    var num_notes = Snare_Array.length;
+    return num_notes;
+}
+
 /*
 	 * midi_output_type:  "general_MIDI" or "Custom"
 	 * num_notes: number of notes in the arrays  (currently expecting 32 notes per measure)
